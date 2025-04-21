d3e0707963197fc8d3f78718ea7c7797191b99c4
baluVijayaKumar <127827386+baluVijayaKumar@users.noreply.github.com>
2025-04-21 19:12:54 +0530

    config url change

--- app/config/env/development.js
+++ app/config/env/development.js
@@ -2,5 +2,5 @@ const port = 1337;
 
 module.exports = {
   port,
-  db: process.env.DB_CONNECTION_URL || 'mongodb://localhost/aktivolabs-web',
+  db: process.env.DB_CONNECTION_URL || 'mongodb://localhost/test',
 };
